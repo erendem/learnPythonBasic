9eb429cae426e17d81fcba1466bfa188fb69da45
erendem <eren.demirdt@gmail.com>
2023-11-08 22:20:43 +0300

    quiz added

---
+++ LearnLooping/quize_exercises.py
@@ -0,0 +1,63 @@
+"""
+Soru 1:
+
+Kullanıcıdan 2 değer alınacak.
+
+İlk alınan değer döngünün uzunluğudur.
+İkinci alınan değer ise sayının kaçar kaçar gideceğidir.
+
+Örnek: döngü uzunluğuna 5 girilsin.
+        Sayılar da 7'şer 7'şer artacak densin(yani ikinci alınan değer 7 olsun)
+
+Kodun çıktısı bir listede ilk girilen sayı 5 olduğu için --> [7,14,21,28,35] yeni liste döndürmeli.
+
+Mesala kullanıcı 3 ve 9 girerse --> [9,18,27] olmalı.
+
+Kullanıcı eğer tam sayı girmez de string girerse hatalı değer girdiniz denilip tekrar kullanıcıdan sayı alınmalı.
+Eğer kullanıcı float, virgüllü sayı girerse integer'a cast edilip yapılması lazım.
+
+"""
+
+
+
+"""
+
+Soru 2:
+
+
+Şu şekilde bir liste tanımlanır:
+
+case_sensitive_keyword = ["helloWorld", "myFunctionRunning", "bigProblem", "easyForLoop", "americanfootball"]
+
+yeni bir listede büyük harfe göre kelimeleri ayırıp ve her şeyi küçük yazarak ayrı ayrı kelimeler halinde liste oluşturulur.
+Eğer büyük harf yoksa hiçbi şey yapılmaz kelime aynen yazılır.
+
+örnek yukardaki liste, döngü ile yeni bir listeye dönüştürülür çıktı olarak --> 
+
+ new_list = ["hello world", "my function running", "big problem", "easy for loop", "americanfootball"] 
+
+
+"""
+
+"""
+Soru 3:
+
+Loves me, Loves me not Oyunu:
+
+Kullanıcıdan döngünün kaç defa döneceğinin sayısı alınır.
+
+girilen sayıya göre seviyor, sevmiyor yazılacaktır. Örnek kullanıcı 5 girdi.
+
+"Loves me, Loves me not, Loves me, Loves me not, LOVES ME" şeklinde yazacaktır. En son değer BÜYÜK HARFLE yazılacaktır. Bu bir liste olmayacak düz bir text olacaktır.
+   1           2            3          4              5
+   
+Örnek 3 girdi --> "Loves me, Loves me not, LOVES ME"
+Örnek 1 sayısını girerse --> "LOVES ME"
+Örnek 2 girerse --> "Loves me, LOVES ME NOT"
+
+
+"""
+
+
+
+
